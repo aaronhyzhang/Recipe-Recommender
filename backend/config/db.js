0192ccf1bf1877caf30afed09163decf19c80bf9
aaronhyzhang <aaron11886z@gmail.com>
2024-01-10 22:41:09 -0800

    Reverted back to old

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,27 +1,12 @@
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI);
-//     console.log(`MongoDB connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error(error);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
-    return conn;
   } catch (error) {
     console.error(error);
-    throw new Error("Failed to connect to MongoDB");
+    process.exit(1);
   }
 };
 
